f820cba8e3a29afd7779848065361745cf0dccc5
brandonjon26 <brandonjon26@gmail.com>
2021-04-06 13:52:42 -0500

    customers are now working! delete button works as well

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,19 +1,23 @@
-import React from "react"
-import "./Animal.css"
+import React from 'react';
+import "./Animal.css";
 
-// animal card representing animals
-export const AnimalCard = ({ animal }) => {
-    return (
-      <div className="card">
-        <div className="card-content">
-          <picture>
-            <img src={require('./dog.svg')} alt="My Dog" />
-          </picture>
-          <h3>Name: <span className="card-petname">
-            {animal.name}
-          </span></h3>
-          <p>Breed: {animal.breed}</p>
+export const AnimalCard = ({ animal, handleDeleteAnimal }) => {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <div className="animals">
+          <div className="animal">
+            <picture>
+              <img src={require('./dog.svg')} alt="My Dog" />
+            </picture>
+            <h3>Name: <span className="card-petname">
+              {animal.name}
+            </span></h3>
+            <p>Breed: {animal.breed}</p>
+            <button type="button" onClick={() => handleDeleteAnimal(animal.id)}>Discharge</button>
+           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- src/components/customer/CustomerCard.js
+++ src/components/customer/CustomerCard.js
@@ -2,9 +2,23 @@ import React from "react";
 import "./Customer.css";
 
 // created card representing customers
-export const CustomerCard = ({ customer }) => (
-    <section className="customer">
-        <h3 className="customer_name">{customer.name}</h3>
-        <div className="customer_address">{customer.address}</div>
-    </section>
-)
+export const CustomerCard = ({ customer, handleDeleteCustomer }) => {
+    return (
+      <div className="card">
+        <div className="card-content">
+            <div className="customer">
+                <div className="customers">
+                    <h3>Name: <span className="card-customername">
+                        {customer.name}
+                    </span></h3>
+                    <p>Address: {customer.address}</p>
+                    <button type="button" onClick={() => handleDeleteCustomer(customer.id)}>Discharge</button>
+                </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+                    // <picture>
+                    // <   img src={require('./dog.svg')} alt="My Dog" />
+                    // </picture>
