0d9bf47bdfda1ff62dfde398674addcc02fa4182
brandonjon26 <brandonjon26@gmail.com>
2021-04-08 11:39:30 -0500

    details properly display on the page

--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { getLocationById, deleteLocation } from '../../modules/LocationManager';
-import { getEmployeeById } from '../../modules/EmployeeManager';
+import { getAllEmployees } from '../../modules/EmployeeManager';
 import './LocationDetail.css';
 import { useParams, useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export const LocationDetail = () => {
-    const [location, setLocation] = useState({});
-    const [employees, setEmployee] = useState({});
+    const [location, setLocation] = useState({employees: []});
+    // const [employees, setEmployees] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     const { locationId } = useParams();
-    const { employeeId } = useParams();
+    // const { employeeId } = useParams();
     const history = useHistory();
 
     useEffect(() => {
@@ -31,24 +31,28 @@ export const LocationDetail = () => {
         );
       };
 
-      useEffect(() => {
-        console.log("useEffect", employeeId)
-        getEmployeeById(employeeId)
-            .then(employee => {
-                console.log(employee);
-                setEmployee(employee);
-                setIsLoading(false);
-            });
-    }, [employeeId]);
+    //   useEffect(() => {
+    //     console.log("useEffect", employeeId)
+    //     getAllEmployees()
+    //         .then(allEmployees => {
+    //             console.log(allEmployees);
+    //             setEmployees(allEmployees);
+    //             setIsLoading(false);
+    //         });
+    // }, [employeeId]);
 
     return (
         <section className="location">
             <h3 className="location__name">{location.name}</h3>
             <div className="location__address">{location.address}</div>
             {/* What's up with the question mark???? See below.*/}
-            {console.log(employees)}
-            <div className="location__employee">Employees: {employees.map(
-                employee => location.id === employee.locationId)}</div>
+            <div className="location__employee">Employees: 
+                {/* {employees.filter(
+                employee => location.id === employee.locationId).map(employee => employee.name)} */}
+                
+                {location.employees.map(employee => employee.name).join(", ")}
+            
+            </div>
             <Link to={`/locations/`}>
                 <button>Back</button>
             </Link>
