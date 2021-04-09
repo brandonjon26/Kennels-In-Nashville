d6535e4f11323a7c6311d741f6c9d1c1c2382ed6
brandonjon26 <brandonjon26@gmail.com>
2021-04-09 13:54:54 -0500

    added edit button and form to customers

--- src/modules/CustomerManager.js
+++ src/modules/CustomerManager.js
@@ -1,17 +1,27 @@
 const remoteURL = "http://localhost:5002"
 
 export const getCustomerById = (id) => {
-    return fetch(`${remoteURL}/customers/${id}?_expand=animal&_expand=location`)
-        .then(res => res.json())
+  return fetch(`${remoteURL}/customers/${id}?_expand=animal&_expand=location`)
+    .then(res => res.json())
 }
 
 export const getAllCustomers = () => {
-    return fetch(`${remoteURL}/customers`)
-        .then(result => result.json())
+  return fetch(`${remoteURL}/customers`)
+    .then(result => result.json())
 }
 
 export const deleteCustomer = (id) => {
-    return fetch(`${remoteURL}/customers/${id}`, {
-      method: "DELETE"
-    }).then(result => result.json())
-  }
+  return fetch(`${remoteURL}/customers/${id}`, {
+    method: "DELETE"
+  }).then(result => result.json())
+}
+
+export const updateCustomer = (editedCustomer) => {
+  return fetch(`${remoteURL}/customers/${editedCustomer.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(editedCustomer)
+  }).then(data => data.json());
+}
