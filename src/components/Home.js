045f1969e42e59a2cb858a7f69869116b346cf25
brandonjon26 <brandonjon26@gmail.com>
2021-04-09 09:59:53 -0500

    added a reload box to display an animal

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AnimalSpotlight } from "../components/animal/AnimalSpotlight"
+import { getRandomId } from "../modules/AnimalManager"
 import { PropsAndState } from "./PropsAndState";
 
 // plug in cards to JSX to display on the DOM
-export const Home = () => (
-    <>
-        <h2>Nashville Kennels</h2>
+
+export const Home = () => {
+    const [spotlightId, setSpotlightId] = useState(0);
+  
+    const refreshSpotlightAnimal = () => {
+      getRandomId().then(setSpotlightId);
+    };
+  
+    useEffect(() => {
+      refreshSpotlightAnimal();
+    }, []);
+  
+    return (
+      <>
+      <h2>Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
 
         <address>
@@ -13,5 +27,18 @@ export const Home = () => (
         </address>
 
         <PropsAndState yourName="Brandon" pet="Samson, Dog" />
-    </>
-)
+
+        <button onClick={refreshSpotlightAnimal}>Reload &#x27f3;</button>
+        {
+          spotlightId && <AnimalSpotlight animalId={spotlightId} />
+        }
+      </>
+    );
+  };
+  
+
+// export const Home = () => (
+//     <>
+        
+//     </>
+// )
